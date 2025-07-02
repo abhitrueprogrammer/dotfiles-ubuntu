c9da593676c20ac0f0d3ace0d46aa1c671f11857
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-07-02 22:15:18 +0000

    fix: nvim font

--- app_config/nvim.sh
+++ app_config/nvim.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 mkdir $HOME/.fonts
-cp -n "./settings/JetBrainsMonoNerdFont-Regular.ttf" $HOME/.fonts
+cp -n "../settings/JetBrainsMonoNerdFont-Regular.ttf" $HOME/.fonts
 fc-cache -fv
