b99d5413450a83f14131b2c9d9a3b83d1e429dda
abhitrueprogrammer <abhiaruvs3@gmail.com>
2025-07-08 04:26:57 +0530

    feat: add keyboard shortcuts for switching workspace

--- app_config/gnome.sh
+++ app_config/gnome.sh
@@ -1,2 +1,10 @@
 gsettings set org.gnome.desktop.interface enable-hot-corners true
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Super>F1']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-2 "['<Super>F2']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-3 "['<Super>F3']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-4 "['<Super>F4']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-5 "['<Super>F5']"
+gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-6 "['<Super>F6']"
+
